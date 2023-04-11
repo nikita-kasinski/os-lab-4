e6782caf2b0818ebd373fd161cbf462e868f91fa
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-11 21:48:56 +0300

    declare movePointer method

--- include/controller.h
+++ include/controller.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <string> 
 
 class Controller
 {
 private:
     std::string binaryFileName;
-    int currentAvailableMessage = 0;
+    int messageHead;
+    int messageTail;
     int messageCount;
+    void movePointer(int& pointer) const;
 public:
     Controller(const std::string& binaryFileName, int messageCount);
     bool postMessage(const std::string& message) const;
