17b74ab24d0ae3ad43945322b734a03714afcc39
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-11 20:37:17 +0300

    add arguments count guard

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello world\n";
+    std::string binaryFile;
+    std::cout << "Input binary file name\n";
+    std::cin >> binaryFile;
     return 0;
 }
--- src/sender.cpp
+++ src/sender.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc != 1)
+    {
+        std::cout << "There must be only one argument: binary file name\n";
+    }
     std::cout << "Hello world\n";
     return 0;
 }
