75fb4bbf12c640f7c54a050383be3add1072eba0
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-12 10:18:59 +0300

    declare getHead, getTail methods

--- include/controller.h
+++ include/controller.h
@@ -5,11 +5,14 @@ class Controller
 {
 private:
     std::string binaryFileName;
-    size_t messageHead;
-    size_t messageTail;
     size_t maxMessageCount;
-    
+    static const size_t maxMessageSize = 20;
+    static const char dummyChar = '0';
+    static const size_t atomicOffset = sizeof(size_t);
+
     void movePointer(size_t& pointer) const;
+    size_t getHead() const;
+    size_t getTail() const;
 public:
     Controller(const std::string& binaryFileName, size_t messageCount);
     bool postMessage(const std::string& message) const;
