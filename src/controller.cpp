a817c6eb166c3deb515bc0e007ce16d16d1da5b6
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-12 10:19:20 +0300

    improve initBinaryFile

--- src/controller.cpp
+++ src/controller.cpp
@@ -8,8 +8,6 @@ Controller::Controller(const std::string &binaryFileName, size_t messageCount)
 {
     this->binaryFileName = binaryFileName;
     this->maxMessageCount = messageCount;
-    this->messageHead = 0;
-    this->messageTail = 0;
 }
 
 void Controller::movePointer(size_t &pointer) const
@@ -27,8 +25,6 @@ void Controller::movePointer(size_t &pointer) const
 void Controller::initBinaryFile(const std::string &binaryFileName, size_t maxMessageCount)
 {
     std::fstream fout(binaryFileName, std::ios::binary | std::ios::out);
-    constexpr int maxMessageSize = 20;
-    constexpr char dummyChar = '0';
     std::string dummyString;
 
     for (size_t i = 0; i < maxMessageSize; ++i)
@@ -36,17 +32,20 @@ void Controller::initBinaryFile(const std::string &binaryFileName, size_t maxMes
         dummyString += dummyChar;
     }
 
+    //first - head, second - tail, then messages
+    fout.write(0, atomicOffset);
+    fout.write(0, atomicOffset);
     for (size_t i = 0; i < maxMessageCount; ++i)
     {
         fout.write(dummyString.c_str(), sizeof(dummyString.c_str()));
     }
 }
+
 bool Controller::postMessage(const std::string &message) const
 {
-    assert(message.length() <= 20);
+    assert(message.length() <= maxMessageSize);
     std::fstream fout(binaryFileName, std::ios::binary | std::ios::out | std::ios::in);
-    fout.seekp(5);
-    fout.write("##", 2);
+    fout.seekp(messageTail * (maxMessageSize + 1));
+    fout.write(message.c_str(), sizeof(message.c_str()));
     fout.close();
-    return false;
 }
