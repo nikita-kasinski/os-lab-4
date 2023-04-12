5e7f1aed1f60648934484319e4f08c1be36c5057
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-12 11:50:12 +0300

    implement messageCount methods, add improved offsets

--- src/controller.cpp
+++ src/controller.cpp
@@ -22,36 +22,76 @@ void Controller::movePointer(size_t &pointer) const
     }
 }
 
-size_t Controller::getHead(std::fstream& fin) const
+size_t Controller::getHead(std::fstream &fin) const
 {
-    fin.seekp(0);
+    fin.seekp(headOffset);
     size_t head;
-    fin.read((char*)&head, atomicSize);
+    fin.read((char *)&head, atomicSize);
     return head;
 }
 
-size_t Controller::getTail(std::fstream& fin) const
+size_t Controller::getTail(std::fstream &fin) const
 {
-    fin.seekp(atomicOffset);
+    fin.seekp(tailOffset);
     size_t tail;
-    fin.read((char*)&tail, atomicSize);
+    fin.read((char *)&tail, atomicSize);
     return tail;
 }
 
-void Controller::moveTail(std::fstream& f) const
+size_t Controller::getMessageCount(std::fstream &fin) const
+{
+    fin.seekp(messageCountOffset);
+    size_t messageCount;
+    fin.read((char *)&messageCount, atomicSize);
+    return messageCount;
+}
+
+void Controller::moveTail(std::fstream &f) const
 {
     size_t tail = getTail(f);
     movePointer(tail);
-    f.seekp(atomicOffset);
-    f.write((char*)&tail, atomicSize);
+    f.seekp(tailOffset);
+    f.write((char *)&tail, atomicSize);
 }
 
-void Controller::moveHead(std::fstream& f) const
+void Controller::moveHead(std::fstream &f) const
 {
     size_t head = getHead(f);
     movePointer(head);
-    f.seekp(0);
-    f.write((char*)&head, atomiceSize);
+    f.seekp(headOffset);
+    f.write((char *)&head, atomicSize);
+}
+
+bool Controller::increaseMessageCount(std::fstream &f) const
+{
+    size_t messageCount = getMessageCount(f);
+    f.seekp(messageCountOffset);
+    if (messageCount == maxMessageCount)
+    {
+        return false;
+    }
+    else
+    {
+        ++messageCount;
+        f.write((char *)&(messageCount), atomicSize);
+        return true;
+    }
+}
+
+bool Controller::decreaseMessageCount(std::fstream &f) const
+{
+    size_t messageCount = getMessageCount(f);
+    f.seekp(messageCountOffset);
+    if (messageCount == 0)
+    {
+        return false;
+    }
+    else
+    {
+        --messageCount;
+        f.write((char*)&(messageCount), atomicSize);
+        return true;
+    }
 }
 
 void Controller::initBinaryFile(const std::string &binaryFileName, size_t maxMessageCount)
@@ -64,9 +104,10 @@ void Controller::initBinaryFile(const std::string &binaryFileName, size_t maxMes
         dummyString += dummyChar;
     }
 
-    //first - head, second - tail, then messages
-    fout.write(0, atomicOffset);
-    fout.write(0, atomicOffset);
+    // first - head, second - tail, third - messageCount,  then messages
+    fout.write(0, atomicSize);
+    fout.write(0, atomicSize);
+    fout.write(0, atomicSize);
     for (size_t i = 0; i < maxMessageCount; ++i)
     {
         fout.write(dummyString.c_str(), sizeof(dummyString.c_str()));
@@ -78,7 +119,7 @@ bool Controller::postMessage(const std::string &message) const
     assert(message.length() <= maxMessageSize);
     std::fstream fout(binaryFileName, std::ios::binary | std::ios::out | std::ios::in);
     size_t tail = getTail(fout);
-    fout.seekp(tail);
+    fout.seekp(overallOffset + tail * (maxMessageSize + 1));
     fout.write(message.c_str(), sizeof(message.c_str()));
     moveTail(fout);
     fout.close();
