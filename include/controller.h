956911764bd36d7de1d11febe30c5eaea40aa3aa
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-12 10:28:36 +0300

    declare moveHead, moveTail methods

--- include/controller.h
+++ include/controller.h
@@ -9,10 +9,19 @@ private:
     static const size_t maxMessageSize = 20;
     static const char dummyChar = '0';
     static const size_t atomicOffset = sizeof(size_t);
+    static const size_t atomicSize = atomicOffset;
 
     void movePointer(size_t& pointer) const;
+
+    //first - head, second - tail
     size_t getHead(std::fstream& fin) const;
+
+    //first - head, second - tail
     size_t getTail(std::fstream& fin) const;
+
+    void moveHead(std::fstream& f) const;
+    
+    void moveTail(std::fstream& f) const;
 public:
     Controller(const std::string& binaryFileName, size_t messageCount);
     bool postMessage(const std::string& message) const;
