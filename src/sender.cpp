f507da125f707b138f8218d9c2f2067b72472a22
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-14 11:33:13 +0300

    fix event and mutex usage in sender

--- src/sender.cpp
+++ src/sender.cpp
@@ -17,10 +17,10 @@ int main(int argc, char** argv)
 
     std::ostringstream eventName;
     eventName << "Sent message " << id;
-    HANDLE sendEvent = OpenEventA(EVENT_MODIFY_STATE, FALSE, eventName.str().c_str());
-    HANDLE readEvent = OpenEventA(EVENT_MODIFY_STATE, FALSE, "Got message");
-    HANDLE iomtx = OpenMutexA(MUTEX_MODIFY_STATE, FALSE, "IO mutex");
-    HANDLE fmtx = OpenMutexA(MUTEX_MODIFY_STATE, FALSE, "File mutex");
+    HANDLE sendEvent = OpenEventA(EVENT_ALL_ACCESS, TRUE, eventName.str().c_str());
+    HANDLE readEvent = OpenEventA(EVENT_ALL_ACCESS, TRUE, "Got message");
+    HANDLE iomtx = OpenMutexA(MUTEX_ALL_ACCESS, FALSE, "IO mutex");
+    HANDLE fmtx = OpenMutexA(MUTEX_ALL_ACCESS, FALSE, "File mutex");
     
     std::string menuPrompt = "Enter respective symbol:\nq to quit\nm to message the receiver\n";
     std::string senderPrompt = "Sender ";
@@ -50,23 +50,25 @@ int main(int argc, char** argv)
             // now we can work with the file safely
             if (ctrl.postMessage(message))
             {
+                ReleaseMutex(fmtx);
                 // message posted successfully
                 std::cout << "Message posted successfully\n";
                 SetEvent(sendEvent);
             }
             else
             {
+                ReleaseMutex(fmtx);
                 std::cout << "Message wasn't posted as file is full.\n";
                 ResetEvent(readEvent);
                 std::cout << "Waiting for message to be read\n";
                 WaitForSingleObject(readEvent, INFINITE); // waiting for message to be read
             }
-            ReleaseMutex(fmtx);
         }
         else
         {
             std::cout << "Unknown option: " << response << ". Try again\n";
         }
+        std::cout << "\n";
     }
     return 0;
 }
