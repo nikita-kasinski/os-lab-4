4db2514225d1d7f33c36a16e2d240695e29fec3a
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-14 07:29:23 +0300

    add events and mutexes retreival, start implementing main menu

--- src/sender.cpp
+++ src/sender.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <windows.h>
+#include <sstream>
 
 int main(int argc, char** argv)
 {
-    if (argc != 1)
+    if (argc != 3)
     {
-        std::cout << "There must be only one argument: binary file name\n";
+        std::cerr << "There must be exactly two args: binary file name and id\n";
+        return 1;
     }
-    std::cout << "Hello world\n";
+    std::string binaryFile = argv[1];
+    size_t id = static_cast<size_t>(atoi(argv[2]));
+    std::ostringstream eventName;
+    eventName << "Sent message " << id;
+    HANDLE sendEvent = OpenEventA(EVENT_MODIFY_STATE, FALSE, eventName.str().c_str());
+    HANDLE readEvent = OpenEventA(EVENT_MODIFY_STATE, FALSE, "Got message");
+    HANDLE iomtx = OpenMutexA(MUTEX_MODIFY_STATE, FALSE, "IO mutex");
+    HANDLE fmtx = OpenMutexA(MUTEX_MODIFY_STATE, FALSE, "File mutex");
+    
+    std::string MenuPrompt = "Enter respective symbol:\nq to quit\n m to message the receiver\n";
+    while(true)
+    {
+        std::cout << MenuPrompt;
+        SetEvent(sendEvent);
+        
+    }
     return 0;
 }
